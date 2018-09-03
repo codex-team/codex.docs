c16894ebf3fd876947b4591b2af230b7b5e8416e
Peter Savchenko <specc.dev@gmail.com>
2018-09-03 21:10:03 +0300

    Frontent build system is ready

---
+++ frontend/js/app.js
@@ -0,0 +1,8 @@
+import css from '../styles/main.pcss';
+
+module.exports = class Docs {
+  constructor(){
+    console.log('CodeX Docs initialized');
+
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+/**
+ * Options for the Babel
+ */
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    cacheDirectory: '.cache/babel-loader',
+    presets: [
+      "env"
+    ],
+    plugins: [
+      /**
+       * Dont need to use «.default» after «export default Class Ui {}»
+       * @see  {@link https://github.com/59naga/babel-plugin-add-module-exports}
+       */
+      // 'add-module-exports',
+    ]
+  }
+};
+
+module.exports = (env) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.p?css$/,
+          use: ExtractTextPlugin.extract([
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: './frontend/'
+                }
+              }
+            }
+          ])
+        }, {
+          test: /\.js$/,
+          use: [
+            babelLoader
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('bundle.css')
+    ],
+    optimization: {
+      minimize: true
+    }
+  };
+};
