3b302cfc46668037ec4a4a166cfc7636596fe381
Peter Savchenko <specc.dev@gmail.com>
2018-09-18 22:20:48 +0300

    Add Editor to the writing page

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
+require('./utils/twig');
 
 app.use(logger('dev'));
 app.use(express.json());
--- src/frontend/js/app.js
+++ src/frontend/js/app.js
@@ -2,11 +2,42 @@
 // eslint-disable-next-line no-unused-vars
 import css from '../styles/main.pcss';
 
-module.exports = class Docs {
+/**
+ * Module Dispatcher
+ * @see {@link https://github.com/codex-team/moduleDispatcher}
+ * @author CodeX
+ */
+import ModuleDispatcher from 'module-dispatcher';
+
+/**
+ * Import modules
+ */
+import Writing from './modules/writing';
+
+/**
+ * Main app class
+ */
+class Docs {
   /**
    * @constructor
    */
   constructor() {
     console.log('CodeX Docs initialized');
+    this.writing = new Writing();
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+      this.docReady();
+    });
   }
-};
+
+  /**
+   * Document is ready
+   */
+  docReady() {
+    this.moduleDispatcher = new ModuleDispatcher({
+      Library: window.Docs
+    });
+  }
+}
+
+module.exports = new Docs();
