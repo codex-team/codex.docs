23abb4584c6cbd633b860d242303280f82a1eff5
Peter Savchenko <specc.dev@gmail.com>
2018-10-19 20:57:37 +0300

    Show created pages in menu

---
+++ src/routes/middlewares/pages.js
@@ -0,0 +1,33 @@
+const Pages = require('../../controllers/pages');
+const asyncMiddleware = require('../../utils/asyncMiddleware');
+
+/**
+ * Process one-level pages list to parent-childrens list
+ * @param {Page[]} pages - list of all available pages
+ * @return {Page[]}
+ */
+function createMenuTree(pages) {
+  return pages.filter(page => page._parent === '0').map(page => {
+    return Object.assign({
+      children: pages.filter(child => child._parent === page._id)
+    }, page.data);
+  });
+}
+
+/**
+ * Middleware for all /page/... routes
+ * @param req
+ * @param res
+ * @param next
+ */
+module.exports = asyncMiddleware(async function (req, res, next) {
+  try {
+    const menu = await Pages.getAll();
+
+    res.locals.menu = createMenuTree(menu);
+  } catch (error) {
+    console.log('Can not load menu:', error);
+  }
+
+  next();
+});
