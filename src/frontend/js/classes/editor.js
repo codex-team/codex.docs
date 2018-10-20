262c1614ed1e8e4367bf90ddafc1a4886a345baf
Peter Savchenko <specc.dev@gmail.com>
2018-10-20 17:54:15 +0300

    New tools added + code highlighting (#12)

--- src/frontend/js/classes/editor.js
+++ src/frontend/js/classes/editor.js
@@ -1,5 +1,9 @@
 import CodeXEditor from 'codex.editor';
 import Header from 'codex.editor.header';
+import CodeTool from 'codex.editor.code';
+import InlineCode from 'codex.editor.inline-code';
+import Marker from 'codex.editor.marker';
+import ListTool from 'codex.editor.list';
 
 /**
  * Class for working with Editor.js
@@ -17,6 +21,19 @@ export default class Editor {
           config: {
             placeholder: 'Enter a title'
           }
+        },
+        code: CodeTool,
+        inlineCode: {
+          class: InlineCode,
+          shortcut: 'CMD+SHIFT+I'
+        },
+        Marker: {
+          class: Marker,
+          shortcut: 'CMD+SHIFT+M'
+        },
+        list: {
+          class: ListTool,
+          inlineToolbar: true
         }
       },
       data: initialData || {
