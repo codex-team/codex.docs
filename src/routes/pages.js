dbfc594e66c4dbe70be7e5e1e9adc860d6e67091
cabad11 <44175180+cabad11@users.noreply.github.com>
2019-01-14 17:53:10 +0300

    Don't allow to set child element as parent. (#19)
    * filter available pages
    
    * refactor
    
    set function as method of Pages
    
    * change wrong word
    
    * change commentaries
    
    * change method name
    
    * refactor
    
    refactor
    
    * Update src/controllers/pages.js
    
    Co-Authored-By: cabad11 <44175180+cabad11@users.noreply.github.com>
    
    * refactor
    
    change comments
    change metod name

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -22,7 +22,7 @@ router.get('/page/edit/:id', async (req, res, next) => {
 
   try {
     let page = await Pages.get(pageId);
-    let pagesAvailable = await Pages.getAll();
+    let pagesAvailable = await Pages.getAllExceptChildrens(pageId);
 
     res.render('pages/form', {
       pagesAvailable,
