e2d412ead39787c2bd0a702da1a28cc61cc4cc9c
Murod Khaydarov <murod.haydarov@gmail.com>
2019-01-25 05:05:08 +0300

    remove log

--- src/routes/api/pages.js
+++ src/routes/api/pages.js
@@ -158,7 +158,6 @@ router.delete('/page/:id', async (req, res) => {
       result: responsePage
     });
   } catch (err) {
-    console.log(err);
     res.status(400).json({
       success: false,
       error: err.message
