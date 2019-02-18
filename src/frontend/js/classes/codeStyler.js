ee6236c0140fb6c2a0585a46b39445d4b3d8309c
Peter Savchenko <specc.dev@gmail.com>
2019-02-18 11:13:17 +0300

    Small layout updates

--- src/frontend/js/classes/codeStyler.js
+++ src/frontend/js/classes/codeStyler.js
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import xml from 'highlight.js/lib/languages/xml';
+import json from 'highlight.js/lib/languages/json';
 import style from 'highlight.js/styles/atom-one-dark.css';
 
 /**
@@ -12,12 +13,13 @@ export default class CodeStyler {
    * @param {string} selector - CSS selector for code blocks
    * @param {string[]} languages - list of languages to highlight, see hljs.listLanguages()
    */
-  constructor({selector, languages = [ 'javascript', 'xml' ]}) {
+  constructor({selector, languages = [ 'javascript', 'xml', 'json' ]}) {
     this.codeBlocksSelector = selector;
     this.languages = languages;
     this.langsAvailable = {
       javascript,
-      xml
+      xml,
+      json
     };
 
     this.init();
