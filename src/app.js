97d7f357ea94171f9118ed4280a0d0a3af8efec5
George Berezhnoy <gohabereg@users.noreply.github.com>
2019-03-06 00:10:49 +0300

    Update src/app.js
    Co-Authored-By: timakasucces <timurkazantaev@gmail.com>

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 require('./utils/twig');
 
-app.use(logger('dev '));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
