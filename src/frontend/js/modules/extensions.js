e26dff268cfaa8f877cb9204d086d8020c0ac79c
DorofeevMark <creekcreek760@gmail.com>
2019-03-13 07:48:12 +0300

    Added misprints

---
+++ src/frontend/js/modules/extensions.js
@@ -0,0 +1,17 @@
+import Misprints from '@codexteam/misprints';
+
+/**
+ * @class Extensions
+ * @classdesc Class for extensions module
+ */
+export default class Extensions {
+  /**
+   * Initialize extensions
+   */
+  constructor() {
+    this.misprints = new Misprints({
+      chatId: 'XXXXXXXX'
+    });
+  }
+
+}
