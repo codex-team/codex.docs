87fa6712544c6446ecd8bec031ed885a52a5dc27
N0str <ntpcp@yandex.ru>
2020-04-20 22:15:36 +0300

    Implement default redirect

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const verifyToken = require('./middlewares/token');
 const router = express.Router();
+const config = require('../../config/index');
 
 /* GET home page. */
 router.get('/', verifyToken, async (req, res) => {
+  if (config.startPage) {
+    return res.redirect(config.startPage);
+  }
   res.render('pages/index', { isAuthorized: res.locals.isAuthorized });
 });
 
