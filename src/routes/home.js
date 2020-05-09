40d5285e5eb13d33550e71c99d1b2dfd324dcceb
Alexander Menshikov <n0str@users.noreply.github.com>
2020-05-09 14:17:29 +0300

    Implement default redirect (#99)
    * Implement default redirect
    
    * Move startPage configuration to env
    
    * Remove console.log
    Change development config back to master
    
    * remove global middleware

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', verifyToken, async (req, res) => {
+  const config = req.app.locals.config;
+  if (config.startPage) {
+    return res.redirect(config.startPage);
+  }
   res.render('pages/index', { isAuthorized: res.locals.isAuthorized });
 });
 
