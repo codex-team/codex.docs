8d9c19e5954446fe3dd76e45e22b58b23ab14cf0
Nikita Melnikov <nikmel2803@gmail.com>
2022-03-06 11:38:59 +0400

    Upgrade packages (#149)
    * update babel, postcss and webpack
    
    * update editor packages
    
    * update linter packages

--- src/backend/exceptions/httpException.ts
+++ src/backend/exceptions/httpException.ts
@@ -5,17 +5,17 @@
  * @property {string} message - detail about the exception
  */
 class HttpException extends Error {
-    public status: number;
-    public message: string;
-    /**
-     * @param status - status of the exception
-     * @param message - message about the exception
-     */
-    constructor(status: number, message: string) {
-      super(message);
-      this.status = status;
-      this.message = message;
-    }
+  public status: number;
+  public message: string;
+  /**
+   * @param status - status of the exception
+   * @param message - message about the exception
+   */
+  constructor(status: number, message: string) {
+    super(message);
+    this.status = status;
+    this.message = message;
+  }
 }
 
 export default HttpException;
