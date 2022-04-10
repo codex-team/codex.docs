ca403f3df3932f933ba182e26fe68f11f31956fa
n0str <team@codex.so>
2022-04-10 14:07:39 +0300

    Use absolute paths for uploads and db

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(`${config.get('uploads')}`));
 
 app.use('/', routes);
 
