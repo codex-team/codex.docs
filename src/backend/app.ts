c8689d331c739e7ebede2771a99679e92ac8a95f
n0str <team@codex.so>
2022-04-10 18:54:28 +0300

    remove space

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 
-
 app.use('/', routes);
 
 // error handler
