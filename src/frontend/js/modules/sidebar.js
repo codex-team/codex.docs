42d0a36e46cf46af60603e938b693fb0340b24a6
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2022-06-07 15:07:06 +0800

    Save state to local storage

--- src/frontend/js/modules/sidebar.js
+++ src/frontend/js/modules/sidebar.js
@@ -1,15 +1,23 @@
 /**
- *
+ * Local storage key
+ */
+const LOCAL_STORAGE_KEY = 'docs_siedabar_state';
+
+/**
+ * Sidebar module
  */
 export default class Sidebar {
   /**
+   * CSS classes
    *
+   * @returns {Record<string, string>}
    */
   static get CSS() {
     return {
       toggler: 'docs-sidebar__section-toggler',
       togglerCollapsed: 'docs-sidebar__section-toggler--collapsed',
       section: 'docs-sidebar__section',
+      sectionAnimated: 'docs-sidebar__section--animated',
       sectionTitle: 'docs-sidebar__section-title',
       sectionTitleActive: 'docs-sidebar__section-title--active',
       sectionList: 'docs-sidebar__section-list',
@@ -17,49 +25,75 @@ export default class Sidebar {
       sectionListItemActive: 'docs-sidebar__section-list-item--active',
     };
   }
+
   /**
-   *
+   * Creates base properties
    */
   constructor() {
     this.nodes = {
       section: null,
     };
-    this.collapsed = {};
+    const storedState = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    this.collapsed = storedState ? JSON.parse(storedState) : {};
   }
 
   /**
-   * @param settings
-   * @param moduleEl
+   * Called by ModuleDispatcher to initialize module from DOM
+   *
+   * @param {writingSettings} settings - module settings
+   * @param {HTMLElement} moduleEl - module element
    */
-  init(settings = {}, moduleEl) {
+  init(settings, moduleEl) {
     this.nodes.sections = Array.from(moduleEl.querySelectorAll('.' + Sidebar.CSS.section));
     this.nodes.sections.forEach(section => {
       const id = section.getAttribute('data-id');
-
-      this.collapsed[id] = false;
       const togglerEl = section.querySelector('.' + Sidebar.CSS.toggler);
 
-      togglerEl.addEventListener('click', e => this.toggleSection(id, section, togglerEl,  e));
+      togglerEl.addEventListener('click', e => this.handleTogglerClick(id, section, togglerEl, e));
+
+      if (typeof this.collapsed[id] === 'undefined') {
+        this.collapsed[id] = false;
+      }
+      if (this.collapsed[id]) {
+        this.setSectionCollapsed(section, togglerEl, true, false);
+      }
     });
   }
 
   /**
-   * @param sectionId
-   * @param sectionEl
-   * @param togglerEl
-   * @param event
+   * Toggles section expansion
+   *
+   * @param {number} sectionId - id of the section to toggle
+   * @param {HTMLElement} sectionEl - section html element
+   * @param {HTMLElement} togglerEl - toggler button html element
+   * @param {MouseEvent} event - click event
+   * @returns {void}
    */
-  toggleSection(sectionId, sectionEl, togglerEl, event) {
+  handleTogglerClick(sectionId, sectionEl, togglerEl, event) {
     event.preventDefault();
-
     this.collapsed[sectionId] = !this.collapsed[sectionId];
-    togglerEl.classList.toggle(Sidebar.CSS.togglerCollapsed, this.collapsed[sectionId]);
-    const sectionList =  sectionEl.querySelector('.' + Sidebar.CSS.sectionList);
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.collapsed));
+    this.setSectionCollapsed(sectionEl, togglerEl, this.collapsed[sectionId]);
+  }
+
+  /**
+   * Updates section's collapsed state
+   *
+   * @param {HTMLElement} sectionEl - element of the section to toggle
+   * @param {HTMLElement} togglerEl - section's toggler button element
+   * @param {boolean} collapsed - new collapsed state
+   * @param {boolean} animated - true if state should change with animation
+   */
+  setSectionCollapsed(sectionEl, togglerEl, collapsed, animated = true) {
+    togglerEl.classList.toggle(Sidebar.CSS.togglerCollapsed, collapsed);
+    const sectionList = sectionEl.querySelector('.' + Sidebar.CSS.sectionList);
 
     if (!sectionList) {
       return;
     }
-    sectionList.classList.toggle(Sidebar.CSS.sectionListCollapsed, this.collapsed[sectionId]);
+    sectionEl.classList.toggle(Sidebar.CSS.sectionAnimated, animated);
+    sectionList.classList.toggle(Sidebar.CSS.sectionListCollapsed, collapsed);
 
     /**
      * Highlight section item as active if active child item is collapsed.
@@ -67,14 +101,15 @@ export default class Sidebar {
     const activeSectionListItem = sectionList.querySelector('.' + Sidebar.CSS.sectionListItemActive);
     const sectionTitle = sectionEl.querySelector('.' + Sidebar.CSS.sectionTitle);
 
-    if (activeSectionListItem) {
-      if (this.collapsed[sectionId]) {
-        setTimeout(() => {
-          sectionTitle.classList.toggle(Sidebar.CSS.sectionTitleActive, true);
-        }, 200);
-      } else {
-        sectionTitle.classList.toggle(Sidebar.CSS.sectionTitleActive, false);
-      }
+    if (!activeSectionListItem) {
+      return;
+    }
+    if (collapsed && animated) {
+      setTimeout(() => {
+        sectionTitle.classList.toggle(Sidebar.CSS.sectionTitleActive, collapsed);
+      }, 200);
+    } else {
+      sectionTitle.classList.toggle(Sidebar.CSS.sectionTitleActive, collapsed);
     }
   }
 }
