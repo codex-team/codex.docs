5c4183b717bb7160ab4b045bd5af3db2819ae1af
slaveeks <81693471+slaveeks@users.noreply.github.com>
2022-07-10 15:21:32 +0300

    Added ability to use custom favicon (#202)
    * Added ability to change favicon in config
    
    * Turned back version of icon in index.twig
    
    * Added opportunity to upload favicon and route to get saved favicon
    
    * Removed favicon from .codexdocsrc.sample
    
    * Added docs to favicon route
    
    * Replaced uploadFavicon to initiating /favicon route, updated function, added catching errors from uploadFile
    
    * Updated Readme, added info about setting up app
    
    * Updated Readme.md
    
    * Some changes
    
    * Favicon data saves to app.locals, replaced uploading favicon to app.ts
    
    * Changed naming in config, from faviconURL to favicon, changed using app.locals variables
    
    * Renamed uploadFavicon to downLoadFavicon, removed log in locals.ts
    
    * Renamed favicon variable in app.ts
    
    * Added checking favicon before uploading function, removed passing locals to views
    
    * Added timeout for uploading favicon request and writeFileSync changed to writeFile
    
    * Removed passing favicon locals and turned back removed variables
    
    * Turned back variables
    
    * Fixed duplicating os.tmpdir
    
    * Fixed braces in objects, added new lines
    
    * Added default favicon path, if favicon does not exists in config
    
    * Updated docs, fixed using local favicon

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -7,10 +7,16 @@ import routes from './routes';
 import HttpException from './exceptions/httpException';
 import * as dotenv from 'dotenv';
 import config from 'config';
+import os from 'os';
+import appConfig from 'config';
+import { downloadFavicon, FaviconData } from './utils/downloadFavicon';
 
 dotenv.config();
 const app = express();
 const localConfig = rcParser.getConfiguration();
+
+// Get url to upload favicon from config
+const favicon: string = appConfig.get('favicon');
 
 app.locals.config = localConfig;
 // view engine setup
@@ -18,12 +24,34 @@ app.set('views', path.join(__dirname, './', 'views'));
 app.set('view engine', 'twig');
 require('./utils/twig');
 
+const downloadedFaviconFolder = os.tmpdir();
+
+// Check if favicon is not empty
+if (favicon) {
+  // Upload favicon by url, it's path on server is '/temp/favicon.{format}'
+  downloadFavicon(favicon, downloadedFaviconFolder).then((res) => {
+    app.locals.favicon = res;
+    console.log('Favicon successfully uploaded');
+  })
+    .catch( (err) => {
+      console.log(err);
+      console.log('Favicon has not uploaded');
+    });
+} else {
+  console.log('Favicon is empty, using default path');
+  app.locals.favicon = {
+    destination: '/favicon.png',
+    type: 'image/png',
+  } as FaviconData;
+}
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use('/uploads', express.static(config.get('uploads')));
+app.use('/favicon', express.static(downloadedFaviconFolder));
 
 app.use('/', routes);
 
